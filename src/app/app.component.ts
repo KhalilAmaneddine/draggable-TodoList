8162b12468ad1bd03fb1f2f1ba574bb0f68434b0
Khalilamaneddine <khalil.amaneddine@gmail.com>
2023-08-21 18:05:48 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialog} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,97 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
-}
+
+  constructor(private dialog: MatDialog, private snackBar: MatSnackBar ) {}
+
+  todo: string[] = [];
+
+  done: string[] = [];
+
+  inPorgress: string[] = [];
+
+  taskValue!: string;
+
+  displayUpdateButton: boolean = false;
+
+  submitted = false;
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  onAddTask(form: NgForm) {
+    this.todo.push(form.value.task);
+    form.reset();
+    this.displayUpdateButton = false;
+  }
+
+  deleteToDo(index: number) {
+    this.todo.splice(index, 1);
+    this.snackBar.open('Task deleted', 'Dismiss', {
+      duration: 3000, 
+    });
+  }
+
+  deleteInProgress(index: number) {
+    this.inPorgress.splice(index, 1);
+    this.snackBar.open('Task deleted', 'Dismiss', {
+      duration: 3000, 
+    });
+  }
+
+  deleteDone(index: number) {
+    this.done.splice(index, 1);
+    this.snackBar.open('Task deleted', 'Dismiss', {
+      duration: 3000, 
+    });
+  }
+
+  onEdit(index: number) {
+    this.displayUpdateButton = true;
+    this.taskValue = this.todo[index];
+    this.todo.splice(index, 1);
+  }
+
+  confirmDeleteInProgress(index: number): void {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.deleteInProgress(index);
+      }
+    });
+  }
+
+  confirmDeleteToDo(index: number): void {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.deleteToDo(index);
+      }
+    });
+  }
+  confirmDeleteDone(index: number): void {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.deleteDone(index);
+      }
+    });
+  }
+
+  
+ }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    DragDropModule,
+    FormsModule,
+    MatDialogModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
